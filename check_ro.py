2178937112b55e27eada28bb2c829a60a2cb071e
kotlinj <kotlinj@kotlinj-debian8.vdi.cz.net>
2017-05-10 14:34:22 +0200

    nagios plugin initial commit

---
+++ check_ro.py
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+import subprocess
+import sys
+
+cmd = 'grep "\sro[\s,]" /proc/mounts'
+
+ignorepaths = ['tmpfs', '/dev/sr0', '172.27.4.68']
+
+output = 'OK\ file system writeable'
+
+proc = subprocess.Popen([cmd,], shell=True, stdout=subprocess.PIPE)
+
+for line in iter(proc.stdout.readline,''):
+        if line.rstrip().partition(' ')[0] not in ignorepaths and line.rstrip().partition(':')[0] not in ignorepaths:
+                output = 'CRITICAL\ detected read only file system ' + line.rstrip()
+                print(line.rstrip().partition(' ')[0])
+                print(output)
+                sys.exit(2)
+        else:
+                pass
+print(output)
+sys.exit(0)
